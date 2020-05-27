7613997b1969b018bf06398907a1500de3826ad5
bayramcicek <mail@bayramcicek.com.tr>
2020-05-27 20:10:38 +0300

    header

---
+++ malloc_new.c
@@ -0,0 +1,8 @@
+// C11 standard
+// created by cicek on Apr 08, 2020 15:42
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "malloc_new.h"
+
---
+++ malloc_new.h
@@ -0,0 +1,10 @@
+// C11 standard
+// created by cicek on May 15, 2020 11:15
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef MALLOC_MALLOC_NEW_H
+#define MALLOC_MALLOC_NEW_H
+
+#endif //MALLOC_MALLOC_NEW_H
